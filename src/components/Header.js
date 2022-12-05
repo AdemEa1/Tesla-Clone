462432d9ad324a0968fab518a67a213ac53fd2b0
Adem <ademet123676@icloud.com>
2022-12-06 00:01:34 +1100

    init commit

---
+++ src/components/Header.js
@@ -0,0 +1,70 @@
+import React from "react";
+import styled from "styled-components";
+import MenuIcon from "@mui/icons-material/Menu";
+
+function Header() {
+  return (
+    <Container>
+      <a>
+        <img src="/Images/logo.svg" alt="" />
+      </a>
+      <Menu>
+        <a href="#">Model S</a>
+        <a href="#">Model 3</a>
+        <a href="#">Model X</a>
+        <a href="#">Model Y</a>
+      </Menu>
+
+      <RightMenu>
+        <a href="#">Shop</a>
+        <a href="#">Tesla Account</a>
+        <CustomMenu />
+      </RightMenu>
+    </Container>
+  );
+}
+
+export default Header;
+
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
+`;
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+  }
+
+  @media(max-width: 768px) {
+    display: none;
+  }
+`;
+
+const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 10px;
+  }
+`;
+
+const CustomMenu = styled(MenuIcon)`
+
+`;
